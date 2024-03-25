1f4bf83147e2ffc7de2950eabdb90a39f6f1279d
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-25 20:39:30 +0100

    Dictionary extension to retrieve class
    GetValueOrDefault throw an error when the dictionary is null

---
+++ Extensions/DictionaryExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace Iconify.Extensions;
+
+public static class DictionaryExtension
+{
+    public static string Class(this Dictionary<string, object> data, string className)
+    {
+        if (data is null)
+            return string.Empty;
+
+        return data.TryGetValue(className, out var value) ? value.ToString()! : string.Empty;
+    }
+}
