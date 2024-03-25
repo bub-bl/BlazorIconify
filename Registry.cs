91702ba1896621c3d46eb3187f977bcbe1fa2424
bub.bl <66354328+bub-bl@users.noreply.github.com>
2024-03-25 21:32:47 +0100

    Add better implementation for Registry

--- Registry.cs
+++ Registry.cs
@@ -2,7 +2,7 @@
 
 namespace Iconify;
 
-internal sealed class Registry(ILocalStorageService LocalStorage)
+public sealed class Registry(ILocalStorageService LocalStorage)
 {
     private const string CachedIconsKey = "cached-icons";
 
@@ -16,14 +16,23 @@ internal sealed class Registry(ILocalStorageService LocalStorage)
         await LocalStorage.SetItemAsync(CachedIconsKey, _icons);
     }
 
+    public async Task<IconMetadata?> GetIcon(string icon)
+    {
+        var icons = await GetCachedIcons();
+        return icons.FirstOrDefault(x => x.Name == icon);
+    }
+
     public async Task Clear()
     {
         _icons.Clear();
         await LocalStorage.RemoveItemAsync(CachedIconsKey);
     }
 
-    private async Task<List<IconMetadata>> GetCachedIcons() =>
-        _icons = await LocalStorage.GetItemAsync<List<IconMetadata>>(CachedIconsKey) ?? [];
+    private async Task<List<IconMetadata>> GetCachedIcons()
+    {
+        if (_icons.Count > 0) return _icons;
+        return _icons = await LocalStorage.GetItemAsync<List<IconMetadata>>(CachedIconsKey) ?? [];
+    }
 
     private bool IsRegistered(string icon) =>
         _icons.Exists(x => x.Name == icon);
